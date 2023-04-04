0d724b56daa4cfca5df7aafb4b650e4d4f9b4fba
luoyiqi <2431782085@qq.com>
2023-04-04 23:53:11 +0800

    初步实现时钟的模拟，暂停，重启功能

--- DSCourseDesign/src/main/java/VTime/Demo.java
+++ DSCourseDesign/src/main/java/VTime/Demo.java
@@ -1,17 +1,17 @@
 package VTime;
 
+import java.beans.VetoableChangeListener;
+import java.io.IOException;
+import java.sql.Time;
+import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.time.Instant;
-import java.time.ZoneId;
-import java.time.ZonedDateTime;
+import java.time.*;
 import java.time.format.DateTimeFormatter;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.Set;
+import java.util.*;
 
 public class Demo {
-    public static void main(String[] args) throws ParseException {
+    public static void main(String[] args) throws ParseException, InterruptedException, IOException {
 //        Date date = new Date(0L);
 //        long time = date.getTime();
 //        time = time+1000L*60*60*24*365;
@@ -44,9 +44,65 @@ public class Demo {
 //        ZonedDateTime zonedDateTime = Instant.now().atZone(ZoneId.of("Asia/Shanghai"));
 //        System.out.println(zonedDateTime);
 
-        ZonedDateTime time  =Instant.now().atZone(ZoneId.of("Asia/Shanghai"));
-        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yy-MM-dd HH:mm:ss EE a");
-        System.out.println(dtf.format(time));
+//        ZonedDateTime time  =Instant.now().atZone(ZoneId.of("Asia/Shanghai"));
+//        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yy-MM-dd HH:mm:ss EE a");
+//        System.out.println(dtf.format(time));
+
+//        LocalDate nowDate = LocalDate.now();
+//        LocalDate date = LocalDate.of(2023, 2, 3);
+//        Month month = date.getMonth();
+//        System.out.println(month);
+
+//        LocalDate now = LocalDate.now();
+//        LocalDate of = LocalDate.of(2000, 1, 1);
+//
+//        Period between = Period.between(now, of);
+//        System.out.println(between);
+//        Calendar Vcalendar = Calendar.getInstance();
+
+//        DateFormat sdf = new SimpleDateFormat("HH:mm:ss");
+//        while(true){
+//            Thread.sleep(1000);
+//            System.out.println(sdf.format(Vcalendar.getTime()));
+//        }
+
+
+//        class TimerTaskTest extends TimerTask{
+//            @Override
+//            public void run() {
+//                Vcalendar.add(Calendar.MINUTE,1);
+//                SimpleDateFormat sdf  = new SimpleDateFormat("HH:mm:ss");
+//                System.out.println("虚拟时间："+sdf.format(Vcalendar.getTime())+"-------"+"实际时间："+
+//                        sdf.format(Calendar.getInstance().getTime()));
+//            }
+//        }
+//
+//
+//        Timer timer = new Timer();
+//        timer.schedule(new TimerTaskTest(),1000L,2000L);
+
+        VirtualTime time = new VirtualTime();
+        time.TimeStart();
+
+        Scanner scanner = new Scanner(System.in);
+        int x = scanner.nextInt();
+
+        if(x==1){
+            System.out.println(x);
+            System.out.println("停停停");
+            System.out.println(time.getTime());
+            time.pause();
+        }
+        int y = scanner.nextInt();
+
+        if(y==2){
+            System.out.println(y);
+            System.out.println("开始开始开始");
+            time.restart();
+        }
+
+
+
 
 
     }
--- DSCourseDesign/src/main/java/VTime/VirtualTime.java
+++ DSCourseDesign/src/main/java/VTime/VirtualTime.java
@@ -1,5 +1,65 @@
 package VTime;
 
+import java.sql.Time;
+import java.text.SimpleDateFormat;
+import java.util.*;
+
 public class VirtualTime {
+    private Calendar calendar = Calendar.getInstance();//用日历表示时间
+    private static int rate = 5;//现实时间rate秒=虚拟时间1小时
+    private Timer timer = new Timer();//定时器
+    int count=1;//临时变量,统计执行次数，定时任务做好后可以
+
+    //构造函数
+    public VirtualTime() {
+        calendar.set(2023,1,18);
+    }
+
+    public String getTime() {
+        SimpleDateFormat sdf = new SimpleDateFormat("yy-MM-dd HH:mm:ss EE");
+        return sdf.format(calendar.getTime());
+
+    }
+
+
+    //定时器内容
+    class MyTimerTask extends TimerTask {
+        @Override
+        public void run() {
+
+
+            calendar.add(Calendar.HOUR_OF_DAY, 1);//时间推进
+
+            //模拟执行任务区
+            System.out.println(getTime() + "   执行任务，循环第" + count++ + "次");
+
+
+        }
+    }
+
+
+    //开启时间,时间获取方式待定，可以手动输入也可也继承上次的时间，默认时间为2023年2月18日0时0分0秒
+    public void TimeStart(int year,int month,int day,int hour) {
+        calendar.set(year,month-1,day,hour,0,0);
+        timer.schedule(new MyTimerTask(),5000L,5000L);
+    }
+
+    public void TimeStart(){
+        this.TimeStart(2023,2,18,0);
+    }
+
+    public void pause() throws InterruptedException {
+        timer.cancel();
+    }
+
+    public void restart(){
+        timer = new Timer();
+        this.TimeStart(calendar.getTime().getYear(),calendar.getTime().getMonth(),calendar.getTime().getDay(),
+                calendar.getTime().getHours());
+    }
+
+
+
+
 
 }
