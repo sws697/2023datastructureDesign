5fc5370e298ba8a210ff31d4f5c5a8ba68bba7b9
phoenix-cat <91886161+phoenix-cat@users.noreply.github.com>
2023-05-09 11:50:13 +0800

    Create Tempo.java

---
+++ DSCourseDesign/src/main/java/TimeTable/Tempo.java
@@ -0,0 +1,19 @@
+/**
+ * 临时事务
+ */
+package TimeTable;
+
+public class Tempo {
+    String name;
+    String location;
+
+    public Tempo() {
+        name = new String();
+        location = new String();
+    }
+
+    public Tempo(String name, String location) {
+        this.name = name;
+        this.location = location;
+    }
+}
